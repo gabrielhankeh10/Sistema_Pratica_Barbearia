dbe521546ceab81ed001d2136c844f581e846026
gabrielhankeh10 <gabrielhanke1999@gmail.com>
2023-05-18 22:00:37 -0300

    Atualizacao

---
+++ Classes/uServicos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema__Renovo_Barber.Classes
+{
+    public class uServicos :uPai
+    {
+        public uServicos()
+        {
+            uPai Pai = new uPai();
+        }
+        public uPai Pai { get; set; }
+
+    }
+}
